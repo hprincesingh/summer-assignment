7f4f4d78fa1266b6fcadcce55aab170767538955
Prince Singh <singh450prince@gmail.com>
2026-06-19 21:50:40 +0530

    day -16 uploaded

---
+++ day -16/program62.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, count, maxCount = 0, element;
+
+    scanf("%d", &n);
+    int arr[n];
+
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for(i = 0; i < n; i++) {
+        count = 1;
+
+        for(j = i + 1; j < n; j++) {
+            if(arr[i] == arr[j])
+                count++;
+        }
+
+        if(count > maxCount) {
+            maxCount = count;
+            element = arr[i];
+        }
+    }
+
+    printf("%d", element);
+
+    return 0;
+}
